50504c5e3dde52bab364ab9ab847d4e6947a100b
Eduardo Laredo <eduardo.fco.laredo.mateos@gmail.com>
2023-03-11 21:31:44 -0800

    fistCommit

---
+++ src/componets/navBar.jsx
@@ -0,0 +1,21 @@
+import '../styles/navBar.css'
+
+const NavBar = () => {
+  return (
+    <>
+      <header className="header">
+        <a href="#" className="logo">Logo</a>
+
+        <nav className="navbar">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
